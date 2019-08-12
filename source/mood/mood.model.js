1673c063b738f6e7b2116d3f8bc497f6062ab415
Thomas <thomaslee9474@gmail.com>
2019-08-12 10:57:32 -0700

    [added] model

---
+++ source/mood/mood.model.js
@@ -0,0 +1,10 @@
+const { Schema, model } = require("mongoose");
+
+// Schema for storing the mood of people about the weather
+const moodSchema = new Schema({
+    mood: String,
+    city: String,
+    weather: Object
+});
+
+module.exports = model("Mood", moodSchema);
